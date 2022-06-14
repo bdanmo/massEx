54be9427517180272aeb23d7ac271e131544974e
Brendan Moran <b.daniel.moran@gmail.com>
2022-06-14 17:41:14 -0400

    Added basic process-stopping functionality. Kinda stinks.

--- massEx.ps1
+++ massEx.ps1
@@ -6,174 +6,190 @@ $VerbosePreference = "continue"
 
 function Green
 {
-    process { Write-Host $_ -ForegroundColor Green }
+	process { Write-Host $_ -ForegroundColor Green }
 }
 
-function Yellow
+function Blue
 {
-    process { Write-Host $_ -ForegroundColor Yellow }
+	process { Write-Host $_ -ForegroundColor Blue}
 }
 
 function Red
 {
-    process { Write-Host $_ -ForegroundColor Red }
+	process { Write-Host $_ -ForegroundColor Red }
 }
 
 function sayBye {
 
-    param ($Code)
+	param ($Code)
 
-    Write-Output "Goodbye! Closing in..."
-        for ($i=3; $i -gt 0; $i--) {
-            Write-Output $i
-            Start-Sleep -Seconds 1
-        }
+	Write-Output "Goodbye! Closing in..."
+		for ($i=3; $i -gt 0; $i--) {
+			Write-Output $i
+			Start-Sleep -Seconds 1
+		}
 
-        Exit($Code)
+		Exit($Code)
 }
  
 function insist {
-    param ($a, $b, $Prompt)
+	param ($a, $b, $Prompt)
 
-    $a = $a.toUpper()
-    $b = $b.toUpper()
-    $reply = Read-Host -Prompt $Prompt
+	$a = $a.toUpper()
+	$b = $b.toUpper()
+	$reply = Read-Host -Prompt $Prompt
 
-    while($a, $b -notcontains $reply.toUpper()){
-	    $reply = Read-Host -Prompt $Prompt
-    }
+	while($a, $b -notcontains $reply.toUpper()){
+		$reply = Read-Host -Prompt $Prompt
+	}
 
-    if ($reply.ToUpper() -eq $a) {
-        return $true
-    } elseif ($reply.ToUpper() -eq $b) {
-        return $false
-    }
+	if ($reply.ToUpper() -eq $a) {
+		return $true
+	} elseif ($reply.ToUpper() -eq $b) {
+		return $false
+	}
 }
 
 function tryStop {
-    param($name)
+	$continue = insist y n "Would you like to search for the process and attempt to stop it? (Y/N) "
+	
+	if ($continue) {
+		#search for processes and stop them
+		#$stopped = $false
+		$pName = Read-Host "Type all or part of a process name to search for running processes "
+		$pList = Get-Process "*$pName*"
 
-    $continue = insist y n "Would you like to search for the process and attempt to stop it? (Y/N) "
+		if ($pList) {
+			Write-Output $pList | Out-Host
 
-    if ($continue) {
-        #search for processes using $name and stop them
-        $pList = Get-Process $name
-        Write-Host $pList
-        
-        return $true
-    } elseif (!$continue) {
-        return $false
-    }
+			while ($true) {
+				try {
+					$Id = read-Host "Type the PID of the process you would like to end, or type 'Q' to quit "
+					if ($Id.toUpper() -eq 'Q') {
+						return $false
+					} else {
+						Stop-Process -Id $Id -ErrorAction stop
+					}		
+				} catch {
+					Write-Output "Cannot stop the process. Try running this program as an administrator." | Red | Out-Host
+					return $false
+				}
+				return $true
+			}
+		} else {
+			Write-Output "No processes found." | Red | Out-Host
+			tryStop
+		}
+	} elseif (!$continue) {
+		return $false
+	}
 }
 
 function uninstallAll {
-    param ($List, $name)
+	param ($List, $name)
 
-    $status = [PSCustomObject]@{
-        error = $false
-        message = ""
-        successCount = 0
-        failCount = 0
-        total = 0
-    }
+	$status = [PSCustomObject]@{
+		error = $false
+		message = ""
+		successCount = 0
+		failCount = 0
+		total = 0
+	}
 
-    function uninstallThis {
-        param ($App)
+	function uninstallThis {
+		param ($App)
 
-        $uninstall = 
-            if( $App.UninstallString -match "^msiexec" ){
-               "$( $App.UninstallString -replace '/I', '/X' ) /qn /norestart"   
-            } else {
-                "$($App.UninstallString)"
-            }
-        
-        Write-Host "Uninstall Path: $uninstall"
-        Write-Host $App
-        $proc = Start-Process -Filepath cmd -ArgumentList "/C", $uninstall -NoNewWindow -PassThru -Wait
+		$uninstall = 
+			if( $App.UninstallString -match "^msiexec" ){
+			   "$( $App.UninstallString -replace '/I', '/X' ) /qn /norestart"   
+			} else {
+				"$($App.UninstallString)"
+			}
+		
+		$proc = Start-Process -Filepath cmd -ArgumentList "/C", $uninstall -NoNewWindow -PassThru -Wait
 
-        if ($proc.ExitCode -eq 0) {
-            #goodtimes
-            Write-Host "Uninstall of $($App.DisplayName) was successful."
-            $status.successCount += 1
-        } elseif ($proc.ExitCode -eq 1603) {
-            #process running
-            Write-Host "$($app.DisplayName) couldn't be uninstalled because it is running." | Red
-            $stopped = tryStop $name
+		if ($proc.ExitCode -eq 0) {
+			#good times
+			Write-Output "Uninstall of $($App.DisplayName) was successful." | Out-Host
+			$status.successCount += 1
+		} elseif ($proc.ExitCode -eq 1603) {
+			#process running
+			Write-Output "$($app.DisplayName) couldn't be uninstalled because it is running." | Red | Out-Host
+			$stopped = tryStop $name
 
-            if($stopped) {
-                Write-Host "Reattempting uninstall of $($App.DisplayName)" | Yellow
-                uninstallThis $App
-            } elseif (!$stopped) {
-                $status.failCount += 1
-                $status.error = $true
-            }
+			if($stopped) {
+				Write-Output "Reattempting uninstall of $($App.DisplayName)" | Blue | Out-Host
+				uninstallThis $App
+			} elseif (!$stopped) {
+				$status.failCount += 1
+				$status.error = $true
+			}
+		} else {
+			#awshucks
+			Write-Error "$($app.DisplayName) could not be uninstalled. Error code: $($proc.ExitCode)."
+			$status.failCount += 1
+			$status.error = $true
+		} 
+		
+		return $proc
+	}
 
-        } else {
-            #awshucks
-            Write-Error "$($app.DisplayName) could not be uninstalled. Error code: $($proc.ExitCode)."
-            $status.failCount += 1
-            $status.error = $true
-        } 
-        
-        return $proc
-    }
+	foreach ($app in $List) {
+		Write-Output "Attempting uninstall of $($app.DisplayName)..." | Blue
+		$status.total += 1
+		$proc = uninstallThis $app         
+	}
 
-    foreach ($app in $List) {
-        Write-Host "Attempting uninstall of $($app.DisplayName)..."
-        $status.total += 1
-        $proc = uninstallThis $app         
-    }
-
-    if ($status.error) {
-        $status.message = "Batch uninstall completed with errors. Failed to uninstall $($status.failCount) of $($status.total) apps."
-        return $status
-    } else {
-        $status.message = "Batch uninstall complete! $($status.successCount) of $($status.total) programs successfully removed."
-        return $status  
-    }
-    
+	if ($status.error) {
+		$status.message = "Batch uninstall completed with errors. Failed to uninstall $($status.failCount) of $($status.total) apps."
+		return $status
+	} else {
+		$status.message = "Batch uninstall complete! $($status.successCount) of $($status.total) programs successfully removed."
+		return $status  
+	}
+	
 }
 
 function searchAndDestroy {
-    function searchAgain {
-        param ($code)
-        $searchAgain = insist y n "Search for another? (Y/N) "
-        if ($searchAgain) {searchAndDestroy}
-        elseif (!$searchAgain) {sayBye $code}
-    }
+	function searchAgain {
+		param ($code)
+		$searchAgain = insist y n "Search for another? (Y/N) "
+		if ($searchAgain) {searchAndDestroy}
+		elseif (!$searchAgain) {sayBye $code}
+	}
 
-    $name = Read-Host -Prompt "Search for all programs where name includes "
-    $RegKeys = @(
-        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
-        'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
-    )
-    $apps = $RegKeys |
-            Get-ChildItem |
-            Get-ItemProperty | 
-            Where-Object{$_.DisplayName -like "*$name*" -and $_.UninstallString}
-    
-    if ($apps) {
-        $appList = $apps | Select-Object -Property DisplayName, Publisher
-        Write-Output $appList | Out-Host
-        $uninst = insist y n "Uninstall these apps? (Y/N) "
+	$name = Read-Host -Prompt "Search for all programs where name includes "
+	$RegKeys = @(
+		'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+		'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+	)
+	$apps = $RegKeys |
+			Get-ChildItem |
+			Get-ItemProperty | 
+			Where-Object{$_.DisplayName -like "*$name*" -and $_.UninstallString}
+	
+	if ($apps) {
+		$appList = $apps | Select-Object -Property DisplayName, Publisher
+		Write-Output $appList | Out-Host
+		$uninst = insist y n "Uninstall these apps? (Y/N) "
 
-        if ($uninst) {
-            $proc = uninstallAll $apps $name
-            if ($proc.error) {
-                Write-Warning $proc.message
-                searchAgain 1
-            } else {
-                Write-Host $proc.message | Green
-                searchAgain 0
-            }
-            
-        } elseif (!$uninst) {
-            SearchAgain 0
-        }
-    } else {
-        Write-Host "No programs with a name containing `"$name`" were found." | Red
-        SearchAgain 0
-    }
+		if ($uninst) {
+			$proc = uninstallAll $apps $name
+			if ($proc.error) {
+				Write-Warning $proc.message
+				searchAgain 1
+			} else {
+				Write-Output $proc.message | Green
+				searchAgain 0
+			}
+			
+		} elseif (!$uninst) {
+			SearchAgain 0
+		}
+	} else {
+		Write-Output "No programs with a name containing `"$name`" were found." | Red
+		SearchAgain 0
+	}
 }
 
 ################
@@ -185,9 +201,9 @@ Start-Sleep -Milliseconds 500
 $continue = insist y n "Do you wish to continue? (Y/N) "
 
 if($continue) {
-    searchAndDestroy
+	searchAndDestroy
 } elseif (!$continue) {
-    sayBye 0
+	sayBye 0
 }
 
 
